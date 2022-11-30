92699db7a2144e9f57863e643df9cab2ea92d9ff
SochnuyFrykt <serebrenikovdanil@gmail.com>
2022-11-30 23:03:36 +0400

    Удаление gitignore

--- Course paper/ClassComand.cs
+++ Course paper/ClassComand.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Course_paper
 {
-	internal class Class1
+	internal class ClassComand
 	{
+		public static void SwitchColorButton(Label button)
+		{
+			button.MouseEnter += (s, n) =>
+				button.ForeColor = Color.FromArgb(149, 149, 149);
+			button.MouseLeave += (s, n) =>
+				button.ForeColor = Color.White;
+		}
 	}
 }
