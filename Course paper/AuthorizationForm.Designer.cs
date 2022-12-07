727777492def58934cf77e611279b68714e81ad8
SochnuyFrykt <serebrenikovdanil@gmail.com>
2022-12-07 19:58:41 +0400

    An authorization form has been made

--- Course paper/AuthorizationForm.Designer.cs
+++ Course paper/AuthorizationForm.Designer.cs
@@ -28,11 +28,16 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AuthorizationForm));
 			this.TopPanel = new System.Windows.Forms.Panel();
+			this.CollapsButton = new System.Windows.Forms.Label();
 			this.CloseButton = new System.Windows.Forms.Label();
 			this.BottomPanel = new System.Windows.Forms.Panel();
-			this.CollapsButton = new System.Windows.Forms.Label();
 			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label3 = new System.Windows.Forms.Label();
 			this.TopPanel.SuspendLayout();
 			this.BottomPanel.SuspendLayout();
 			this.SuspendLayout();
@@ -40,8 +45,10 @@
 			// TopPanel
 			// 
 			this.TopPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(37)))), ((int)(((byte)(80)))));
+			this.TopPanel.Controls.Add(this.label3);
 			this.TopPanel.Controls.Add(this.CollapsButton);
 			this.TopPanel.Controls.Add(this.CloseButton);
+			this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
 			this.TopPanel.Location = new System.Drawing.Point(0, 0);
 			this.TopPanel.Name = "TopPanel";
 			this.TopPanel.Size = new System.Drawing.Size(350, 30);
@@ -49,9 +56,23 @@
 			this.TopPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseDown);
 			this.TopPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseMove);
 			// 
+			// CollapsButton
+			// 
+			this.CollapsButton.AutoSize = true;
+			this.CollapsButton.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.CollapsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.CollapsButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.CollapsButton.Location = new System.Drawing.Point(301, 4);
+			this.CollapsButton.Name = "CollapsButton";
+			this.CollapsButton.Size = new System.Drawing.Size(17, 18);
+			this.CollapsButton.TabIndex = 1;
+			this.CollapsButton.Text = "_";
+			this.CollapsButton.Click += new System.EventHandler(this.CollapsButton_Click);
+			// 
 			// CloseButton
 			// 
 			this.CloseButton.AutoSize = true;
+			this.CloseButton.Cursor = System.Windows.Forms.Cursors.Hand;
 			this.CloseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.CloseButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
 			this.CloseButton.Location = new System.Drawing.Point(326, 6);
@@ -64,22 +85,12 @@
 			// 
 			this.BottomPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(37)))), ((int)(((byte)(80)))));
 			this.BottomPanel.Controls.Add(this.label1);
-			this.BottomPanel.Location = new System.Drawing.Point(0, 418);
+			this.BottomPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.BottomPanel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+			this.BottomPanel.Location = new System.Drawing.Point(0, 420);
 			this.BottomPanel.Name = "BottomPanel";
 			this.BottomPanel.Size = new System.Drawing.Size(350, 30);
 			this.BottomPanel.TabIndex = 1;
-			// 
-			// CollapsButton
-			// 
-			this.CollapsButton.AutoSize = true;
-			this.CollapsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.CollapsButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-			this.CollapsButton.Location = new System.Drawing.Point(301, 4);
-			this.CollapsButton.Name = "CollapsButton";
-			this.CollapsButton.Size = new System.Drawing.Size(17, 18);
-			this.CollapsButton.TabIndex = 1;
-			this.CollapsButton.Text = "_";
-			this.CollapsButton.Click += new System.EventHandler(this.CollapsButton_Click);
 			// 
 			// label1
 			// 
@@ -92,12 +103,60 @@
 			this.label1.TabIndex = 0;
 			this.label1.Text = "Помощь";
 			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold);
+			this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.label2.Location = new System.Drawing.Point(122, 93);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(106, 20);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Авторизация";
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(119)))), ((int)(((byte)(0)))));
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold);
+			this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.button1.Location = new System.Drawing.Point(110, 280);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(130, 37);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Войти";
+			this.button1.UseVisualStyleBackColor = false;
+			// 
+			// panel1
+			// 
+			this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+			this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.panel1.Cursor = System.Windows.Forms.Cursors.Default;
+			this.panel1.Location = new System.Drawing.Point(110, 126);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(130, 130);
+			this.panel1.TabIndex = 4;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold);
+			this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.label3.Location = new System.Drawing.Point(3, 5);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(122, 20);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "FortuneBuilding";
+			// 
 			// AuthorizationForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(29)))), ((int)(((byte)(58)))));
 			this.ClientSize = new System.Drawing.Size(350, 450);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
 			this.Controls.Add(this.BottomPanel);
 			this.Controls.Add(this.TopPanel);
 			this.Cursor = System.Windows.Forms.Cursors.Default;
@@ -109,6 +168,7 @@
 			this.BottomPanel.ResumeLayout(false);
 			this.BottomPanel.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -119,6 +179,10 @@
 		private System.Windows.Forms.Label CloseButton;
 		private System.Windows.Forms.Label CollapsButton;
 		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label3;
 	}
 }
 
