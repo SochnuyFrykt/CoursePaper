5be40beb990245271dc92de9b48b8aa27a9aa7c0
alt2211 <ruthlessspiritofdarkness@gmail.com>
2022-12-07 21:03:43 +0400

    Created SalaryCalculation form

---
+++ Course paper/SalaryCalculation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Course_paper
+{
+    public partial class SalaryCalculation : Template
+    {
+        Form formtoopen;
+        public SalaryCalculation(MainMenu form)
+        {
+            InitializeComponent();
+            //formtoopen = form;
+        }
+        public override void ButtonBack_Click(object sender, EventArgs e)
+        {
+            formtoopen.Show();
+            Hide();
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            object st = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value;
+        }
+    }
+}
