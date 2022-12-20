371d595225875beceaa046d6e50c96f5fa05ebf8
Kuzkovi4 <kuzkokuzkovi4@gmail.com>
2022-12-20 19:58:15 +0400

    Rec MsTest

--- Course paper test/UnitTest1.cs
+++ Course paper test/UnitTest1.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StrWriter;
 using System;
+using System.IO;
+using System.Windows.Forms;
 
 namespace Course_paper_test
 {
@@ -9,6 +12,66 @@ namespace Course_paper_test
         [TestMethod]
         public void TestMethod1()
         {
+            DataGridView dgv = new DataGridView();
+            string path = "Tables\\testprog.base";
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 5; j++)
+                    dgv.Rows[i].Cells[j].Value = i + j + 1;
+
+            StreamWriter1.Save(dgv, path);
+
+            //Assert.AreEqual(FileCompare());
         }
+        private bool FileCompare(string file1, string file2)
+        {
+            int file1byte;
+            int file2byte;
+            FileStream fs1;
+            FileStream fs2;
+
+            // Determine if the same file was referenced two times.
+            if (file1 == file2)
+            {
+                // Return true to indicate that the files are the same.
+                return true;
+            }
+
+            // Open the two files.
+            fs1 = new FileStream(file1, FileMode.Open);
+            fs2 = new FileStream(file2, FileMode.Open);
+
+            // Check the file sizes. If they are not the same, the files
+            // are not the same.
+            if (fs1.Length != fs2.Length)
+            {
+                // Close the file
+                fs1.Close();
+                fs2.Close();
+
+                // Return false to indicate files are different
+                return false;
+            }
+
+            // Read and compare a byte from each file until either a
+            // non-matching set of bytes is found or until the end of
+            // file1 is reached.
+            do
+            {
+                // Read one byte from each file.
+                file1byte = fs1.ReadByte();
+                file2byte = fs2.ReadByte();
+            }
+            while ((file1byte == file2byte) && (file1byte != -1));
+
+            // Close the files.
+            fs1.Close();
+            fs2.Close();
+
+            // Return the success of the comparison. "file1byte" is
+            // equal to "file2byte" at this point only if the files are
+            // the same.
+            return ((file1byte - file2byte) == 0);
+        }
+
     }
 }
