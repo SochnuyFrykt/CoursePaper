0dfb0d1efe18b7768cff88f735a101d7e50726bf
Kuzkovi4 <kuzkokuzkovi4@gmail.com>
2022-12-20 22:42:37 +0400

    Added tests 2

--- Course paper test/UnitTest1.cs
+++ Course paper test/UnitTest1.cs
@@ -4,12 +4,82 @@ using System;
 using System.IO;
 using System.Windows.Forms;
 using Course_paper;
+using System.Security.Cryptography;
 
 namespace Course_paper_test
 {
     [TestClass]
     public class StreamWriter1Test1
     {
+
+        [TestMethod]
+        public void SaveTest2()
+        {
+            StreamWriter filePathArrange = new StreamWriter(Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName, "Tables\\testprog.base"), false);
+            StreamWriter filePathActual = new StreamWriter(Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName, "Tables\\testTest.base"), false);
+            DataGridView dgv = new DataGridView();
+            dgv.Columns.Add("1", "11");
+            dgv.Columns.Add("2", "22");
+            dgv.Columns.Add("3", "33");
+            dgv.Columns.Add("4", "44");
+            dgv.Columns.Add("5", "55");
+            for (int i = 0; i < 5; i++)
+            {
+                dgv.Rows.Add();
+                for (int j = 0; j < 5; j++)
+                {
+                    dgv.Rows[i].Cells[j].Value = i + j + 1;
+                    filePathActual.WriteLine(i + j + 1);
+                }
+            }
+            filePathArrange.Close();
+            filePathActual.Close();
+
+            StreamWriter1.Save(dgv, "Tables\\testprog.base");
+
+            Assert.AreEqual(ComputeMD5Checksum("Tables\\testTest.base"), ComputeMD5Checksum("Tables\\testprog.base"));
+        }
+        [TestMethod]
+        public void LoadTest2()
+        {
+            StreamWriter filePathArrange = new StreamWriter(Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName, "Tables\\testprog1.base"), false);
+            StreamWriter filePathMedium = new StreamWriter(Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName, "Tables\\testMedium.base"), false);
+            StreamWriter filePathActual = new StreamWriter(Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName, "Tables\\testTest1.base"), false);
+            DataGridView dgv = new DataGridView();
+            dgv.Columns.Add("1", "11");
+            dgv.Columns.Add("2", "22");
+            dgv.Columns.Add("3", "33");
+            dgv.Columns.Add("4", "44");
+            dgv.Columns.Add("5", "55");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    filePathArrange.WriteLine(i + j + 3);
+                    filePathActual.WriteLine(i + j + 3);
+                }
+            }
+            filePathArrange.Close();
+            filePathMedium.Close();
+            filePathActual.Close();
+
+            StreamWriter1.Load(dgv, "Tables\\testprog1.base");
+            StreamWriter1.Save(dgv, "Tables\\testMedium.base");
+
+            Assert.AreEqual(ComputeMD5Checksum("Tables\\testTest1.base"), ComputeMD5Checksum("Tables\\testMedium.base"));
+        }
+        private string ComputeMD5Checksum(string path)
+        {
+            using (FileStream fs = File.OpenRead(Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName, path)))
+            {
+                MD5 md5 = new MD5CryptoServiceProvider();
+                byte[] fileData = new byte[fs.Length];
+                fs.Read(fileData, 0, (int)fs.Length);
+                byte[] checkSum = md5.ComputeHash(fileData);
+                string result = BitConverter.ToString(checkSum).Replace("-", String.Empty);
+                return result;
+            }
+        }
         [TestMethod]
         public void SaveTest1()
         {
@@ -37,7 +107,7 @@ namespace Course_paper_test
             Assert.AreEqual(a1, path);
         }
         [TestMethod]
-        public void LoadTest2()
+        public void LoadTest1()
         {
             string path = @"D:\temp.base";
             string str1 = "", str2 = "";
